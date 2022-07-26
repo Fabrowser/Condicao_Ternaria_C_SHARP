53fb3fecf90e64e5e29a93ded5c4aa194373e92d
Fabricio Guimarães <79614829+Fabrowser@users.noreply.github.com>
2022-07-26 20:22:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ternaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            double desconto = (preco < 20) ? preco * 0.10 : preco * 0.05;
+
+
+            Console.WriteLine("Valor total:" + (preco - desconto));
+
+
+
+
+        }
+    }
+}
